86cf16206f5a272e370f611d412971b6111e3ebf
emanuele5180 <macina5180>
2020-06-25 17:27:24 +0200

    inizio bonus

---
+++ bonus/main.js
@@ -0,0 +1,27 @@
+//Per l'esercizio di oggi, scrivi un programma che stampi i numeri da 1
+//a 100.
+//Per i multipli di 3 stampi “Fizz” al posto del numero.
+//Per i multipli di 5 stampi "Buzz" al posto del numero.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
+
+
+
+
+for (var i = 1; i < 101; i++) {
+
+  if (i%15==0) {
+    console.log("FizzBuzz");
+
+  }else if (i%5 == 0 )  {
+    console.log("Buzz");
+
+  }else if (i%3 == 0 ) {
+    console.log("Fizz");
+  }
+  else {
+    console.log(i);
+  }
+}
+
+
+document.getElementById("FizzBuzz").innerHTML= "<li> + i + </li>";
