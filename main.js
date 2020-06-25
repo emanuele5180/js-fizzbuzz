03064c23fbb5ad9c593aac2d1b11ce0ce9e6ce43
emanuele5180 <macina5180>
2020-06-25 17:06:47 +0200

    FizzBuzz!!!!!

--- main.js
+++ main.js
@@ -5,8 +5,20 @@
 //Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
 
 
-for (var i = 0; i < 100; i++) {
-  var numeri = [];
 
-  console.log(numeri[i]);
+
+for (var i = 1; i < 101; i++) {
+
+  if (i%15==0) {
+    console.log("FizzBuzz");
+
+  }else if (i%5 == 0 )  {
+    console.log("Buzz");
+
+  }else if (i%3 == 0 ) {
+    console.log("Fizz");
+  }
+  else {
+    console.log(i);
+  }
 }
