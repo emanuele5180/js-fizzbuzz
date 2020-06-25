1c207f7f8417f23550a8993e3b8eed3fd87b3eb6
emanuele5180 <macina5180>
2020-06-25 15:30:50 +0200

    numeri

---
+++ main.js
@@ -0,0 +1,12 @@
+//Per l'esercizio di oggi, scrivi un programma che stampi i numeri da 1
+//a 100.
+//Per i multipli di 3 stampi “Fizz” al posto del numero.
+//Per i multipli di 5 stampi "Buzz" al posto del numero.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
+
+
+for (var i = 0; i < 100; i++) {
+  var numeri = [];
+
+  console.log(numeri[]);
+}
